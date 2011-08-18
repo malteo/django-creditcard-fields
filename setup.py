6e7c3ed365c0fc27818f5d8cb21045148bf8fa37
MakeToLearn <jordan@aace.org>
2011-08-18 16:50:44 -0400

    setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-creditcard-fields',
+      version='1.0',
+      description='Credit Card Fields for Django',
+      author='Jordan Reiter, based on Bryan Chow',
+      author_email='jordanreiter@gmail.com',
+      url='https://github.com/JordanReiter/django-creditcard-fields',
+      packages=['django_creditcard'],
+     )
